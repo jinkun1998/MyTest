1099f7f586d47abf874e7c5aafbf8775cc09b8d6
Quoc Thao <quocthao23061998.tg@gmail.com>
2021-08-18 13:59:59 +0700

    bỏ đi constructor thừa trong Context

--- Public/Contexts/UserDbContext.cs
+++ Public/Contexts/UserDbContext.cs
@@ -9,7 +9,6 @@ namespace Public.Entities.Contexts
 {
     public class UserDbContext : DbContext
     {
-        public UserDbContext() { }
         public UserDbContext(DbContextOptions<UserDbContext> options) : base (options) { }
 
         public DbSet<User> Users { get; set; }
